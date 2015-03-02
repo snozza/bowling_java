890ebd33d16107cdc78b7aa68116a1c58fc1cd05
Andrew Snead <andrewcsnead@gmail.com>
2015-03-02 15:33:29 +0000

    basic folder structure setup

---
+++ src/test/java/com/bowling/BowlingGameTest.java
@@ -0,0 +1,12 @@
+package bowling;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class BowlingGameTest {
+
+  @Test
+  public void testGutterGame() throws Exception {
+    Game g = new Game();
+  }
+}
