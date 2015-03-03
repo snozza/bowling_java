b3d4b14d89035d7d5c0e678aebed28f93e61446a
Andrew Snead <andrewcsnead@gmail.com>
2015-03-03 08:07:30 +0000

    first two tests passing

--- src/main/java/com/bowling/Game.java
+++ src/main/java/com/bowling/Game.java
@@ -1,11 +1,13 @@
 package com.bowling;
 
 public class Game {
+  private int score = 0;
 
   public void roll(int pins) {
+    score += pins;
   }
 
   public int score() {
-    return 0;
+    return score;
   }
 }
--- src/test/java/com/bowling/BowlingGameTest.java
+++ src/test/java/com/bowling/BowlingGameTest.java
@@ -1,15 +1,30 @@
 package com.bowling;
 
 import org.junit.Test;
+import org.junit.Before;
 import static org.assertj.core.api.Assertions.*;
 
 public class BowlingGameTest {
+  private Game g;
+
+  @Before
+  public void beforeGameTest() {
+    g = new Game();
+  }
 
   @Test
   public void testGutterGame() throws Exception {
     Game g = new Game();
-    for(int i=0; i < 20; i++)
+    for(int i = 0; i < 20; i++)
       g.roll(0);
-    assertThat(0).isEqualTo(g.score());
+    assertThat(g.score()).isEqualTo(0);
+  }
+
+  @Test
+  public void testAllOnes() throws Exception {
+    Game g = new Game();
+    for (int i = 0; i < 20; i++)
+      g.roll(1);
+    assertThat(g.score()).isEqualTo(20);
   }
 }
